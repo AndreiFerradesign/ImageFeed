a9d93057182b906fc1c213fb636caf5d1b2b3065
AndreiFerradesign <ferradesign@yandex.ru>
2023-01-03 00:21:26 +0300

    Фотография центруется, суммируется и шарится через кнопку, изображение сохраняется в файлах.

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -12,7 +12,6 @@ final class ProfileViewController: UIViewController {
     @IBOutlet private var nameLabel: UILabel!
     @IBOutlet private var loginNameLabel: UILabel!
     @IBOutlet private var descriptionLabel: UILabel!
-
     @IBOutlet private var logoutButton: UIButton!
 
     @IBAction private func didTapLogoutButton() {
