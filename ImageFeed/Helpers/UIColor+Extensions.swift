7a0e9eb5e7e615cde0ac9e81b3bbc70b89677b51
AndreiFerradesign <ferradesign@yandex.ru>
2023-01-03 22:30:17 +0300

    Выполнены задания 9  спринта, экран профиля сверстан кодом. Приложение запускается, экраны переключаются по tabBar, картинки увеличиваются, отцентрованы, иконка пошарить на своем месте, картинки шарятся.

--- ImageFeed/Helpers/UIColor+Extensions.swift
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -11,7 +11,7 @@ extension UIColor {
     static var ypBackground: UIColor {UIColor (named: "YP Background (iOS)")!}
     static var ypBlack: UIColor {UIColor (named: "YP Black (iOS)")!}
     static var ypBlue: UIColor {UIColor (named: "YP Blue (iOS)")!}
-    static var ypGrey: UIColor {UIColor (named: "YP Grey (iOS)")!}
+    static var ypGray: UIColor {UIColor (named: "YP Gray (iOS)")!}
     static var ypRed: UIColor {UIColor (named: "YP Red (iOS)")!}
     static var ypWhite50: UIColor {UIColor (named: "YP White (Alpha 50) (iOS)")!}
     static var ypWhite: UIColor {UIColor (named: "YP White (iOS)")!}
--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -13,10 +13,64 @@ final class ProfileViewController: UIViewController {
     @IBOutlet private var loginNameLabel: UILabel!
     @IBOutlet private var descriptionLabel: UILabel!
     @IBOutlet private var logoutButton: UIButton!
-
+    
     @IBAction private func didTapLogoutButton() {
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initProfileStoryboard()
+    }
+    
+    private func initProfileStoryboard() {
+        let profileImage = UIImage(named: "Avatar")
+        let imageView = UIImageView(image: profileImage)
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(imageView)
+            imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40).isActive = true
+            imageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+            imageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        let nameLabel = UILabel()
+            nameLabel.text = "Екатерина Новикова"
+            nameLabel.textColor = .ypWhite
+            nameLabel.font = UIFont.systemFont(ofSize: 23)
+            nameLabel.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(nameLabel)
+            nameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+            nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
+        
+        let loginNameLabel = UILabel()
+            loginNameLabel.text = "@ekaterina_nov"
+            loginNameLabel.textColor = .ypGray
+            loginNameLabel.font = UIFont.systemFont(ofSize: 13)
+            loginNameLabel.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(loginNameLabel)
+            loginNameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+            loginNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+        
+        let descriptionLabel = UILabel()
+            descriptionLabel.text = "Hello, world!"
+            descriptionLabel.textColor = .ypWhite
+            descriptionLabel.font = UIFont.systemFont(ofSize: 13)
+            descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(descriptionLabel)
+            descriptionLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+            descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8).isActive = true
+        
+        let button = UIButton.systemButton(
+            with: UIImage(systemName: "ipad.and.arrow.forward")!,
+            target: self,
+            action: #selector(Self.didTapLogoutButton)
+        )
+            button.tintColor = .ypRed
+            button.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(button)
+            button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -26).isActive = true
+            button.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 20).isActive = true
+            button.heightAnchor.constraint(equalToConstant: 22).isActive = true
+    }
 }
-
 
