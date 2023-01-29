ea345a095383672d88903014210394d62278eec4
AndreiFerradesign <ferradesign@yandex.ru>
2023-01-29 17:13:29 +0100

    Реализовал 2 вьюконтроллера, подключил WebView, окно авторизации от Unsplash грузиться, кнопка назад на окне авторизации работает, переходит в первую вьюху.

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Andrei on 29.01.2023.
+//
+
+import Foundation
+
+let accessKey = "XirHst7O-9XCZTZLyvRuiSpbpRLiqs8aIqwwYOTv5d4"
+let secretKey = "DQRLVOUF8i8ue9ul_KWgU0qIYSnW4ak0tulO8aVYGDA"
+let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let accessScope = "public+read_user+write_likes"
+let defaultBaseURL = URL(string: "https://api.unsplash.com")!
